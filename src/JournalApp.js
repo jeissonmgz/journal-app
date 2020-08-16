9345e60d0cf96c449d63e1fca1047c373c0d36fb
jeisson-gml <jeissonm@gmlsoftware.com>
2020-08-15 21:14:44 -0500

    JM Routes

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,9 +1,6 @@
 import React from "react";
+import { AppRouter } from "./components/routers/AppRouter";
 
 export const JournalApp = () => {
-  return (
-    <div>
-      <h1>JournalApp</h1>
-    </div>
-  );
+  return <AppRouter />;
 };
