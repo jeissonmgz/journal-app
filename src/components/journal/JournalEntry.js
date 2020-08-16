0f6286f334cb54ebb15c1b685f0e2aa681fb89fa
jeisson-gml <jeissonm@gmlsoftware.com>
2020-08-16 18:23:10 -0500

    JM Styles in journal

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const JournalEntry = () => {
+  return (
+    <div className="journal__entry pointer">
+      <div
+        className="journal__entry-picture"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage:
+            "url('https://www.w3schools.com/w3css/img_lights.jpg')",
+        }}
+      ></div>
+      <div className="journal__entry-body">
+        <p className="journal__entry-title">Un nuevo día</p>
+        <p className="journal__entry-content">
+          Hello, hello, hoy es nuevo día para comenzar.
+        </p>
+      </div>
+      <div className="journal__entry-date-box">
+        <span>Monday</span>
+        <h4>28</h4>
+      </div>
+    </div>
+  );
+};
