802005837e0dd6b3bece4b5a07707d645b618303
jeisson-gml <jeissonm@gmlsoftware.com>
2020-08-17 17:13:09 -0500

    JM Add provider and fix reduxers

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from "redux";
-import { authReducer } from "../reducers/authreducer";
+import { authReducer } from "../reducers/authReducer";
 
 const reducers = combineReducers({
   auth: authReducer,
